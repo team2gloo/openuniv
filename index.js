e50e3b626300d12d624d00827eb8f8fddb7cdebf
EC2 Default User <ec2-user@ip-172-26-2-81.ap-northeast-2.compute.internal>
2020-04-28 18:28:59 +0900

    update

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ $(document).ready(function () {
     $(window).resize(function () {
         $(".ui-autocomplete").hide();
     });
-    fetch('./data.xlsx?version=0426').then((res) => {
+    fetch('./data.xlsx?version=0428').then((res) => {
         res.arrayBuffer().then((ab) => {
             let data = XLSX.read(ab, { type: "array" });
             sheetNames = data.SheetNames;
